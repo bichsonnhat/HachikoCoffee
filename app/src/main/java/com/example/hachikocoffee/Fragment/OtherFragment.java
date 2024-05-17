b5c5d94185ad5ed3269541f8447d81b7dde28b9f
Bich Son Nhat <55039261+bichsonnhat@users.noreply.github.com>
2024-05-17 13:23:45 +0700

    update: resolve conflict, add dialog pakage

--- app/src/main/java/com/example/hachikocoffee/Fragment/OtherFragment.java
+++ app/src/main/java/com/example/hachikocoffee/Fragment/OtherFragment.java
@@ -1,5 +1,7 @@
 package com.example.hachikocoffee.Fragment;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -10,6 +12,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.example.hachikocoffee.Activity.ContactFeedbackActivity;
+import com.example.hachikocoffee.Activity.SavedAddressActivity;
+import com.example.hachikocoffee.Activity.UpdateInfoActivity;
+import com.example.hachikocoffee.Login;
 import com.example.hachikocoffee.NotificationDetail;
 import com.example.hachikocoffee.R;
 import com.example.hachikocoffee.YourVoucher;
