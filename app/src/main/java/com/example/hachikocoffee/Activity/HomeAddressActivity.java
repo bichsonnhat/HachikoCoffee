f180d4ada0fecfb1a40342828784138062d300cf
Bảo Lingg <baolinh02112004@gmail.com>
2024-05-23 21:16:21 +0700

    add event btnHomeAddress

--- app/src/main/java/com/example/hachikocoffee/Activity/HomeAddressActivity.java
+++ app/src/main/java/com/example/hachikocoffee/Activity/HomeAddressActivity.java
@@ -1,19 +1,57 @@
 package com.example.hachikocoffee.Activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import com.example.hachikocoffee.Domain.AddressDomain;
+import com.example.hachikocoffee.Domain.UserDomain;
+import com.example.hachikocoffee.InfoAccountLoginActivity;
+import com.example.hachikocoffee.Listener.OnAddressChangedListener;
 import com.example.hachikocoffee.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.UUID;
 
 public class HomeAddressActivity extends AppCompatActivity {
-
+    private int UserID;
+    TextView tvHomeAddressName;
+    EditText etHomeAddress, etHomeAddressBuilding, etHomeAddressGate, etHomeAddressNote, etHomeAddressReceiverName, etHomeAddressReceiverPhone;
+    Button btnHomeAddressSave;
+    private static OnAddressChangedListener onAddressChangedListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        SharedPreferences perf = getApplication().getSharedPreferences("User", Context.MODE_PRIVATE);
+        UserID = perf.getInt("UserID", 1);
         setContentView(R.layout.activity_home_address);
+        tvHomeAddressName = findViewById(R.id.homeAddressName);
+        etHomeAddress = findViewById(R.id.homeAddress);
+        etHomeAddressBuilding = findViewById(R.id.homeAdressBuilding);
+        etHomeAddressGate = findViewById(R.id.homeAddressGate);
+        etHomeAddressNote = findViewById(R.id.homeAddressNote);
+        etHomeAddressReceiverName = findViewById(R.id.homeAddressReceiverName);
+        etHomeAddressReceiverPhone = findViewById(R.id.homeAddressReceiverPhone);
+        btnHomeAddressSave = findViewById(R.id.btnHomeAddressSave);
+        btnHomeAddressSave.setEnabled(false);
 
         Button btnHomeAddressBack = findViewById(R.id.btnHomeAddressBack);
         btnHomeAddressBack.setOnClickListener(new View.OnClickListener() {
@@ -22,5 +60,127 @@ public class HomeAddressActivity extends AppCompatActivity {
                 onBackPressed();
             }
         });
+
+        DatabaseReference userRef = FirebaseDatabase.getInstance().getReference().child("USER");
+
+        userRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()){
+                    for (DataSnapshot issue : snapshot.getChildren()){
+                        UserDomain user = issue.getValue(UserDomain.class);
+                        if (user.getUserID() == UserID){
+                            String fullname = user.getName();
+                            String[] name = fullname.split(",");
+                            etHomeAddressReceiverName.setText(name[0]);
+                            etHomeAddressReceiverPhone.setText(user.getPhoneNumber());
+                            break;
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        etHomeAddress.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (etHomeAddress.getText().toString().isEmpty()){
+                    etHomeAddress.setError("Vui lòng nhập địa chỉ!");
+                }
+                checkAllFieldsRequire();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        etHomeAddressReceiverName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (etHomeAddressReceiverName.getText().toString().isEmpty()){
+                    etHomeAddressReceiverName.setError("Vui lòng nhập tên người nhận!");
+                }
+                checkAllFieldsRequire();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        etHomeAddressReceiverPhone.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (etHomeAddressReceiverPhone.getText().toString().isEmpty()){
+                    etHomeAddressReceiverPhone.setError("Vui lòng nhập số điện thoại người nhận!");
+                }
+                checkAllFieldsRequire();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        btnHomeAddressSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(HomeAddressActivity.this);
+                builder.setTitle("Thông báo");
+                builder.setMessage("Bạn đã thêm địa chỉ nhà thành công!");
+
+                builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        String homeAddress = etHomeAddress.getText().toString() + ", " + etHomeAddressBuilding.getText().toString() + ", " + etHomeAddressGate.getText().toString() + ", " + etHomeAddressNote.getText().toString();
+                        String userNameAndTelephone = etHomeAddressReceiverName.getText().toString() + " " + etHomeAddressReceiverPhone.getText().toString();
+
+                        // Trả kết quả về activity trước
+                        Intent resultIntent = new Intent(HomeAddressActivity.this, SavedAddressActivity.class);
+                        resultIntent.putExtra("show_home_address", true);
+                        resultIntent.putExtra("Address", homeAddress);
+                        resultIntent.putExtra("NameAndTelephone", userNameAndTelephone);
+                        setResult(Activity.RESULT_OK, resultIntent);
+                        finish();
+                    }
+                });
+
+                AlertDialog alertDialog = builder.show();
+            }
+        });
+    }
+
+    private void checkAllFieldsRequire() {
+        if (!etHomeAddress.getText().toString().isEmpty() && !etHomeAddressReceiverName.getText().toString().isEmpty() && !etHomeAddressReceiverPhone.getText().toString().isEmpty()) {
+            btnHomeAddressSave.setBackground(ContextCompat.getDrawable(this, R.drawable.background_color));
+            btnHomeAddressSave.setEnabled(true);
+        } else {
+            btnHomeAddressSave.setBackground(ContextCompat.getDrawable(this, R.drawable.rounded_rectangle_darkgrey));
+            btnHomeAddressSave.setEnabled(false);
+        }
     }
 }
--- app/src/main/java/com/example/hachikocoffee/Activity/SavedAddressActivity.java
+++ app/src/main/java/com/example/hachikocoffee/Activity/SavedAddressActivity.java
@@ -1,10 +1,22 @@
 package com.example.hachikocoffee.Activity;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.core.view.ViewCompat;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -15,10 +27,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.hachikocoffee.Adapter.AddressAdapter;
 import com.example.hachikocoffee.Domain.AddressDomain;
+import com.example.hachikocoffee.Fragment.HomeAddressFragment;
 import com.example.hachikocoffee.Listener.OnAddressChangedListener;
 import com.example.hachikocoffee.Login;
 import com.example.hachikocoffee.NotificationDetail;
@@ -33,13 +48,60 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 import static com.example.hachikocoffee.Activity.NewAddressActivity.setInterfaceInstance;
 
+
+
 public class SavedAddressActivity extends AppCompatActivity implements OnAddressChangedListener {
     private RecyclerView recyclerView;
     private int UserID = 1;
+    private ActivityResultLauncher<Intent> startForResult = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK && result.getData() != null) {
+                        Intent data = result.getData();
+                        String homeAddress = data.getStringExtra("Address");
+                        String userNameAndTelephone = data.getStringExtra("NameAndTelephone");
+
+                        // Tạo bundle và đặt dữ liệu vào
+                        Bundle bundle = new Bundle();
+                        bundle.putString("Address", homeAddress);
+                        bundle.putString("NameAndTelephone", userNameAndTelephone);
+
+                        // Tạo Fragment và setArguments(bundle)
+                        HomeAddressFragment fragment = new HomeAddressFragment();
+                        fragment.setArguments(bundle);
+
+                        // Thực hiện thay thế fragment trong container
+                        FragmentManager fragmentManager = getSupportFragmentManager();
+                        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                        fragmentTransaction.replace(R.id.rl_home_address, fragment);
+                        fragmentTransaction.commit();
+                    }
+                }
+            });
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_saved_address);
+
+//        RelativeLayout rlHomeAddress = findViewById(R.id.rl_home_address);
+        boolean replaceHomeAddress = getIntent().getBooleanExtra("show_home_address", false);
+        if (savedInstanceState == null) {
+            if (replaceHomeAddress) {
+                // Ẩn RelativeLayout
+//                rlHomeAddress.setVisibility(View.GONE);
+
+//                Thêm HomeAddressFragment vào FragmentTransaction
+                Fragment HomeAddressFragment = new HomeAddressFragment();
+                getSupportFragmentManager().beginTransaction().replace(R.id.rl_home_address, HomeAddressFragment).commit();
+                TextView vhAddressNameAddress = findViewById(R.id.vh_address_nameAddress);
+                TextView vhAddressDetailAddress = findViewById(R.id.vh_address_detailAddress);
+                TextView vhAddressUserNameAndTelephone = findViewById(R.id.vh_address_userNameAndTelephone);
+            }
+        }
+
         recyclerView = findViewById(R.id.recycler_view_saved_addresses);
         SharedPreferences perf = getSharedPreferences("User", Context.MODE_PRIVATE);
         UserID = perf.getInt("UserID", 1);
@@ -55,7 +117,7 @@ public class SavedAddressActivity extends AppCompatActivity implements OnAddress
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(SavedAddressActivity.this, HomeAddressActivity.class);
-                startActivity(intent);
+                startForResult.launch(intent);
             }
         });
 
@@ -113,5 +175,4 @@ public class SavedAddressActivity extends AppCompatActivity implements OnAddress
         recyclerView.clearFocus();
         initAddress();
     }
-
 }
