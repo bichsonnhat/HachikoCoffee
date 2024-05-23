1d029ac8c0be339a738cb50a7841826954af4dbe
Bich Son Nhat <55039261+bichsonnhat@users.noreply.github.com>
2024-05-23 22:53:15 +0700

    update: description advertisement

--- app/src/main/java/com/example/hachikocoffee/PhotoDetail.java
+++ app/src/main/java/com/example/hachikocoffee/PhotoDetail.java
@@ -4,12 +4,21 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.hachikocoffee.Domain.AdvertisementDomain;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class PhotoDetail extends AppCompatActivity {
     ImageView closeBtn;
-
+    private TextView AdvertisementDescription;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,6 +47,26 @@ public class PhotoDetail extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 finish();
+            }
+        });
+
+        AdvertisementDescription = findViewById(R.id.AdvertisementDescription);
+        DatabaseReference advertisementRef = FirebaseDatabase.getInstance().getReference().child("ADVERTISEMENT");
+        advertisementRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    AdvertisementDomain advertisement = dataSnapshot.getValue(AdvertisementDomain.class);
+                    if (advertisement.getAdID() == position + 1){
+                        AdvertisementDescription.setText(""+advertisement.getDescription());
+                        break;
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
             }
         });
 
