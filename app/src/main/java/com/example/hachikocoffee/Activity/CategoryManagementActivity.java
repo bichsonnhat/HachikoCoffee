6bf6afd2afe6ed3434007fd762c338ed8ec92371
h0angpc <118001661+h0angpc@users.noreply.github.com>
2024-05-28 23:59:29 +0700

    update: finish UI for Category

---
+++ app/src/main/java/com/example/hachikocoffee/Activity/CategoryManagementActivity.java
@@ -0,0 +1,16 @@
+package com.example.hachikocoffee.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.hachikocoffee.R;
+
+public class CategoryManagementActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category_management);
+    }
+}
