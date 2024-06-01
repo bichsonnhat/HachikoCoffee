a9b3deb3de23003c65ef5b8793b2698bb2915d72
Bich Son Nhat <55039261+bichsonnhat@users.noreply.github.com>
2024-06-01 19:37:11 +0700

    feat: add voucher

---
+++ app/src/main/java/com/example/hachikocoffee/Activity/EditVoucherActivity.java
@@ -0,0 +1,16 @@
+package com.example.hachikocoffee.Activity;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.hachikocoffee.R;
+
+public class EditVoucherActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_voucher);
+    }
+}
