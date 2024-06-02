509d7d57556378189af1a37c9bbde124e5010cd1
Bich Son Nhat <55039261+bichsonnhat@users.noreply.github.com>
2024-06-03 01:38:29 +0700

    feat: implement product management (add, edit, delete)

--- app/src/main/java/com/example/hachikocoffee/Activity/DashboardActivity.java
+++ app/src/main/java/com/example/hachikocoffee/Activity/DashboardActivity.java
@@ -2,15 +2,38 @@ package com.example.hachikocoffee.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 
 import com.example.hachikocoffee.R;
 
 public class DashboardActivity extends AppCompatActivity {
-
+    private Button btn_product;
+    private Button btn_voucher;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        btn_voucher = findViewById(R.id.btn_voucher);
+        btn_product = findViewById(R.id.btn_product);
+
+        btn_product.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DashboardActivity.this, ProductManagementActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_voucher.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DashboardActivity.this, VoucherManagementActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/hachikocoffee/Activity/ProductManagementActivity.java
+++ app/src/main/java/com/example/hachikocoffee/Activity/ProductManagementActivity.java
@@ -1,26 +1,88 @@
 package com.example.hachikocoffee.Activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
+import com.example.hachikocoffee.Adapter.EditProductAdapter;
+import com.example.hachikocoffee.Adapter.EditVoucherAdapter;
+import com.example.hachikocoffee.Domain.ItemsDomain;
+import com.example.hachikocoffee.Listener.Callback;
 import com.example.hachikocoffee.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-public class ProductManagementActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import static com.example.hachikocoffee.Activity.AddProductActivity.setEditInterfaceInstance;
 
+public class ProductManagementActivity extends AppCompatActivity implements Callback {
+    private ConstraintLayout constraintLayoutStart;
+    private RecyclerView recyclerViewProduct;
     private ImageView btnBack;
+    private ArrayList<ItemsDomain> itemsDomainArrayList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_management);
+        constraintLayoutStart = findViewById(R.id.constraintLayoutStart);
+        recyclerViewProduct = findViewById(R.id.recyclerViewProduct);
         btnBack = findViewById(R.id.back_button);
+        constraintLayoutStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setEditInterfaceInstance(ProductManagementActivity.this);
+                Intent intent = new Intent(ProductManagementActivity.this, AddProductActivity.class);
+                startActivity(intent);
+            }
+        });
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                onBackPressed();
+                finish();
             }
         });
+
+        initProduct();
+    }
+
+    private void initProduct() {
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.VERTICAL, false);
+        recyclerViewProduct.setLayoutManager(linearLayoutManager);
+        itemsDomainArrayList = new ArrayList<>();
+        DatabaseReference productRef = FirebaseDatabase.getInstance().getReference().child("PRODUCTS");
+        productRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()){
+                    for (DataSnapshot issue : snapshot.getChildren()){
+                        ItemsDomain itemsDomain = issue.getValue(ItemsDomain.class);
+                        itemsDomainArrayList.add(itemsDomain);
+                    }
+                }
+                EditProductAdapter editProductAdapter = new EditProductAdapter(itemsDomainArrayList);
+                recyclerViewProduct.setAdapter(editProductAdapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onCallback() {
+        itemsDomainArrayList.clear();
+        initProduct();
     }
 }
